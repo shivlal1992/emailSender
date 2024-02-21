e5322d1c563de1c81d0d5c5b9855c511a8bc8750
shivlal <azadshiv1992@gmail.com>
2024-02-21 14:50:17 +0530

    first commit

--- index.js
+++ index.js
@@ -1,55 +1,57 @@
-const express=require('express');
-const app= express();
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 
-//CREATE the middileware for the parsing request bodies 
+//create the middleware for the parsing requested bodies
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended:true}));
 
-//define to the server that the static files are stored inside the public
+//define to the server that the static files are stored inside the public folder.
 
 app.use(express.static('public'));
 
 //defining the route for home page
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/public/send-email.html');
-})
+});
+
 
 //configure nodemailer
-const nodemailer=require('nodemailer');
-const transporter=nodemailer.createTransport({
-    service:'gmail',
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
     auth:{
-        user:'azadshiv1992@gmail.com',
-        password:'comm muep szfo obeo'
-
+        user: 'azadshiv1992@gmail.com',
+        pass: 'commmuepszfoobeo',
     }
-
 });
 
-//create the route for 
+//create the route for the form
 app.post('/send-email',(req,res)=>{
-    const {to,subject,text} =req.body;
+    const {to, subject,text}=req.body;
 
-    const mailOptions={
+    const mailOptions = {
+        from:'azadshiv1992@gmail.com',
         to,
         subject,
-        message
+        text,
     };
+
     transporter.sendMail(mailOptions,(error,infor)=>{
         if(error){
-            console.error(error);
-            res.status(500).send('error in sending mail');
+            console.log(error);
+            res.status(500).send('Error in sending mail');
         }
         else{
-            console.log('email Send:'+ infor.response);
-            res.send('email send Successfully');
+            console.log('Email Send: '+infor.response);
+            res.send('Email send successfully!');
         }
     });
+
 });
 
 
-//start the server with specific port
-app.listen(port,()=>{
-    console.log(`Server is running on port  ${port}`)
+//start ther server
+app.listen (port,()=>{
+    console.log(`server is running on the port ${port}`);
 });
